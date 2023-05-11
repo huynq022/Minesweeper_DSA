1f1d60a377ceb5f92d0b82355e0c1966a6781c79
Tuvy-ititiu20125 <91869179+Tuvy-ititiu20125@users.noreply.github.com>
2023-05-11 10:55:08 +0700

    Upload file GamePlay.java

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,1 +1,13 @@
+package com.zetcode;
+import java.awt.*;
+public class GamePlay {
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            var ex = new Minesweeper();
+            ex.setVisible(true);
+        });
+    }
+}
 
