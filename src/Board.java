e4c6c6c734f0529fa9218e11bd3722fd643ec0ef
Nguyễn Hoàng Lý <86660377+ly12345680@users.noreply.github.com>
2023-05-11 10:53:13 +0700

    add the properties of boad

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,20 @@
 public class Board{
-    
+    private final int NUM_IMAGES = 13;
+    private final int CELL_SIZE = 15;
+
+    private final int COVER_FOR_CELL = 10;
+    private final int MARK_FOR_CELL = 10;
+    private final int EMPTY_CELL = 0;
+    private final int MINE_CELL = 9;
+    private final int COVERED_MINE_CELL = MINE_CELL + COVER_FOR_CELL;
+    private final int MARKED_MINE_CELL = COVERED_MINE_CELL + MARK_FOR_CELL;
+
+    private final int DRAW_MINE = 9;
+    private final int DRAW_COVER = 10;
+    private final int DRAW_MARK = 11;
+    private final int DRAW_WRONG_MARK = 12;
+
+    private final int N_MINES = 40;
+    private final int N_ROWS = 16;
+    private final int N_COLS = 16;
 }
